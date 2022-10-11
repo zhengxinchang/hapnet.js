7d94cea3ce9bd33235987bec50f76b3aaa09d505
zhengxinchang <zhengxinchang1993@163.com>
2022-10-11 11:20:10 +0800

    fix scroll boundary detection.

--- src/drawToolTip.js
+++ src/drawToolTip.js
@@ -24,126 +24,141 @@ SOFTWARE.
 import * as PIXI from 'pixi.js'
 import store from './store'
 
-
+/**
+ * Class of tooltip
+ */
 class toolTip extends PIXI.Container {
 
-    constructor(options){
-        super();
-        this.options = options;
-        this.name = "hapnet_menu";
-        //sub objects must be initialized in the constructor(), otherwise, accidents will occur!
-        this.chartBackGround = this.addChild(new PIXI.Graphics())
-        this.chartText = this.addChild( new PIXI.Text());
-        this.boundMask = this.addChild(new PIXI.Graphics());
+  /**
+   * @constructor
+   * @param {Object} options options to construct tooltip
+   */
+  constructor(options) {
+    super();
+    this.options = options;
+    this.name = "hapnet_menu";
+    //sub objects must be initialized in the constructor(), otherwise, accidents will occur!
+    this.chartBackGround = this.addChild(new PIXI.Graphics())
+    this.chartText = this.addChild(new PIXI.Text());
+    this.boundMask = this.addChild(new PIXI.Graphics());
 
-        /* turn on the interactive mode */
-        this.interactive = true;
-        this.buttonMode = true;
-        /* set mouse status */
-        this.on('mouseover', (event) => {
-            // console.log(event)
-            store.runtimeGlobal.mouseStatus.onToolTip = true;
-        });
-        this.on('mouseout', (event) => {
-            // console.log(event)
-            store.runtimeGlobal.mouseStatus.onToolTip = false;
-        });
+    /* turn on the interactive mode */
+    this.interactive = true;
+    this.buttonMode = true;
 
-                /* init parameters */
-        this.scaleBorderWidth = store.runtimeGlobal.initOption.width * 0.002 ;
-        this.toolTipWidth = store.runtimeGlobal.initOption.width *0.2 
-        this.toolTipHeight = store.runtimeGlobal.initOption.height *0.5  
-        this.toolTipFontSize = store.runtimeGlobal.plotOption.toolTip.fontSize  ;
-        this.toolTipBackGroundColor = store.runtimeGlobal.plotOption.toolTip.backgroundColor;
-        this.toolTipBorderColor = store.runtimeGlobal.plotOption.toolTip.borderColor;
+    /* set mouse status */
+    this.on('mouseover', (event) => {
+      store.runtimeGlobal.mouseStatus.onToolTip = true;
+    });
+    this.on('mouseout', (event) => {
+      store.runtimeGlobal.mouseStatus.onToolTip = false;
+    });
+
+    /* init parameters */
+    this.scaleBorderWidth = store.runtimeGlobal.initOption.width * 0.002;
+    this.toolTipWidth = store.runtimeGlobal.initOption.width * 0.2
+    this.toolTipHeight = store.runtimeGlobal.initOption.height * 0.5
+    this.toolTipFontSize = store.runtimeGlobal.plotOption.toolTip.fontSize;
+    this.toolTipBackGroundColor = store.runtimeGlobal.plotOption.toolTip.backgroundColor;
+    this.toolTipBorderColor = store.runtimeGlobal.plotOption.toolTip.borderColor;
+    /* init padding of text on the left and top */
+    this.paddingLeft = this.toolTipWidth * 0.02;
+    this.paddingTop = this.toolTipHeight * 0.02;
+
+  }
+
+  clear() {
+
+    console.log('toolTip code cleaning....')
+
+  }
+
+  scroll(deltaFixed) {
 
 
+    const scrollContentDelta = this.toolTipHeight * deltaFixed / 30;
+    const nextYPosition = this.chartText.y + scrollContentDelta;
+    const scrollContent = Math.max((this.textMetrics.lineHeight * this.textMetrics.lines.length), this.toolTipHeight)
+    // console.log(`
+    // this.chartText.y:${this.chartText.y},
+    // deltaFixed:${deltaFixed},
+    // this.textMetrics.height:${this.textMetrics.height},
+    // this.toolTipHeight ${this.toolTipHeight}
+    // nextYPosition ${nextYPosition}
+    // this.paddingTop ${this.paddingTop}
+    // scrollContent ${scrollContent}
+    // `)
+    // console.log(this.textMetrics)
+
+    /* use delta to compare two value */
+
+    if (nextYPosition > 0) {
+      if (Math.abs(nextYPosition - this.paddingTop) < 0.01) {
+        this.chartText.y = this.paddingTop
+      }
+    } else {
+
+      /*
+          TODO:
+          set bottom border detection.
+      */
+      this.chartText.y = nextYPosition
     }
-
-    clear(){
-
-        
-        console.log('toolTip code cleaning....')
-
-    }
-
-    scroll(deltaFixed){
-        console.log(`
-        this.chartText.y:${this.chartText.y},
-        deltaFixed:${deltaFixed},
-        this.textMetrics.height:${this.textMetrics.height},
-        (this.chartText.y + Math.max(this.textMetrics.height,this.toolTipHeight)) :${(this.chartText.y + Math.max(this.textMetrics.height,this.toolTipHeight)) }
-        `)
-        // this.chartBackGround.y +=  this.toolTipHeight * deltaFixed /100
-        // if(this.chartText.y >= 0 && deltaFixed < 0){
-            
-        //     this.chartText.y +=  this.toolTipHeight * deltaFixed /100
-        // }else if( this.chartText.y < 0 && deltaFixed <0  ){
-            
-        // }
-
-        if( this.chartText.y >= this.toolTipWidth *0.02 || (Math.abs(this.chartText.y) - Math.max(this.textMetrics.height,this.toolTipHeight)) > this.toolTipWidth *0.02 ){
-
-        }else{
-            this.chartText.y +=  this.toolTipHeight * deltaFixed /100
-        }
-        
-        // this.chartText.y +=  this.toolTipHeight * deltaFixed /100
-    }
-        
-
-    setAndShow(node,posX,posY){
+  }
 
 
+  setAndShow(node, posX, posY) {
 
-        this.x = posX+1;
-        this.y = posY+1;
-        this.chartBackGround.x = 0;
-        this.chartBackGround.y = 0;
-        this.chartBackGround.visible = true;
-        this.zIndex = 2;
 
-        // the position of this.chartBackGround is based on parent container. 
-        this.chartBackGround
-        .beginFill(this.toolTipBackGroundColor, 1)
-        .lineStyle(this.scaleBorderWidth, this.toolTipBorderColor)
-        .moveTo(0,0)
-        .lineTo(this.toolTipWidth,0)
-        .lineTo(this.toolTipWidth,this.toolTipHeight)
-        .lineTo(0,this.toolTipHeight)
-        .lineTo(0,0)
-        .endFill();
+    /* offset one px as padding */
+    this.x = posX + 1;
+    this.y = posY + 1;
+    this.chartBackGround.x = 0;
+    this.chartBackGround.y = 0;
+    this.chartBackGround.visible = true;
+    this.zIndex = 2;
 
-        const  style = new PIXI.TextStyle({
-            fill: store.runtimeGlobal.plotOption.toolTip.fontColor,
-            fontSize: this.toolTipFontSize ,
-            breakWords:true,
-            wordWrap:true,
-            wordWrapWidth:this.toolTipWidth *0.95 ,
-            // width:toolTipWidth ,
-        });
-        // let textMetrics = PIXI.TextMetrics.measureText('hello woo000000000000000000000000000000oorld', style)
-        this.chartText.x = this.toolTipWidth *0.02;
-        this.chartText.y = this.toolTipHeight *0.02;
-        this.chartText.resolution = 2 ;
-        this.chartText.zIndex = 4;
-        this.chartText.style =  style
+    /* the position of this.chartBackGround is based on parent container. */
+    this.chartBackGround
+      .beginFill(this.toolTipBackGroundColor, 1)
+      .lineStyle(this.scaleBorderWidth, this.toolTipBorderColor)
+      .moveTo(0, 0)
+      .lineTo(this.toolTipWidth, 0)
+      .lineTo(this.toolTipWidth, this.toolTipHeight)
+      .lineTo(0, this.toolTipHeight)
+      .lineTo(0, 0)
+      .endFill();
 
-        const toolTipText = `ID: ${node.id}\n\nRadius: ${node.radius}\n\nComposition: \n${
-            node.sectors.map(d=>{
-                return d.category +": "+d.number.toFixed(4)
-            }).join("\n")
-        }\n
+    const style = new PIXI.TextStyle({
+      fill: store.runtimeGlobal.plotOption.toolTip.fontColor,
+      fontSize: this.toolTipFontSize,
+      breakWords: true,
+      wordWrap: true,
+      wordWrapWidth: this.toolTipWidth * 0.95,
+      // width:toolTipWidth ,
+    });
+    // let textMetrics = PIXI.TextMetrics.measureText('hello woo000000000000000000000000000000oorld', style)
+    this.chartText.x = this.paddingLeft;
+    this.chartText.y = this.paddingTop;
+    this.chartText.resolution = 2;
+    this.chartText.zIndex = 4;
+    this.chartText.style = style
+
+    const toolTipText = `ID: ${node.id}\n\nRadius: ${node.radius}\n\nComposition: \n${
+      node.sectors.map(d => {
+        return d.category + ": " + d.number.toFixed(4)
+      }).join("\n")
+    }\n
         asdfasdaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
         `
 
-        this.chartText.text = toolTipText;
-        // this.chartText.width = toolTipWidth *0.95
-        this.chartText.visible = true;
+    this.chartText.text = toolTipText;
+    // this.chartText.width = toolTipWidth *0.95
+    this.chartText.visible = true;
 
-        this.textMetrics = PIXI.TextMetrics.measureText(toolTipText, style)
-        
-        // console.log(textMetrics)
+    this.textMetrics = PIXI.TextMetrics.measureText(toolTipText, style)
+
+    // console.log(textMetrics)
         // Add the rectangular area to show
         this.boundMask.beginFill(0xff19ff,1)
         .moveTo(0,0)
